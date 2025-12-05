67f39b954a57a28675a4e1a0c86e1f704963766e
Juha Jermalainen <juha.jermalainen@gmail.com>
2025-12-05 23:58:38 +0200

    tehtävä 19

--- viikko 6/app.js
+++ viikko 6/app.js
@@ -18,3 +18,42 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+//middleware
+app.use('/app',
+    function(req,res,next){
+        console.log('The example middleware called');
+        next();
+    }
+);
+//get- metodi
+app.get('/app', (req, res) => res.send('Saanko esitellä'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+//get- metodi yhdellä parametrilla
+app.get("/app/:num", (req, res) => {
+
+  res.send('appin numero ' + req.params.num);
+});
+//get- metodi kahdella parametrilla
+app.get("/app/:num/:name", (req, res) => {
+
+    res.send('saanko esitellä ' + req.params.name + ' ' + req.params.num);
+});
+//get bonus
+
+app.get('/app/:num/:name/:img', (req, res) => {
+  const { num, name, img } = req.params;
+
+
+
+  const imgUrl = `/images/${img}`;
+
+
+  res.send('saanko esitellä ' + req.params.name + ' ' + req.params.num + `<br><img src="${imgUrl}" alt="${name}">`);
+});
+
+//post- metodi
+app.post('/post', (req, res) => {
+    const data = req.body;
+    res.send(`Received data: ${JSON.stringify(data)}`);
+    });
